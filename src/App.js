681551f0bdd36eeba74d2fc0651a57ae546c5e49
= <=>
2020-11-26 18:00:42 +0530

    added-infinite scrolling

--- src/App.js
+++ src/App.js
@@ -2,36 +2,37 @@ import React,{useState,useEffect} from 'react'
 import './App.css';
 import { UnSplashImages } from './components/UnSplashImages';
 import axios from 'axios';
+import { Loading } from './components/Loading';
 
-
+import InfiniteScroll from 'react-infinite-scroll-component'
 // 7_3vMLQaq-cRWFvkq2mJwwc7tpeGofkpXvo-jnZqVcM
 function App() {
 const [images,setImages] = useState([])
 
 useEffect(()=>{
-  const apiUrl = "https://api.unsplash.com/photos/?client_id="
-  const accessKey = "7_3vMLQaq-cRWFvkq2mJwwc7tpeGofkpXvo-jnZqVcM";
-  axios.get(`${apiUrl}${accessKey}&count=10`)
-  .then(res => setImages([...images,...res.data]))
+fetchImages()
 },[])
-// console.log(images)
+// https://source.unsplash.com/random
+// https://api.unsplash.com/photos/?client_id=
+const fetchImages = (count=10)=>{
+  const apiUrl = "https://api.unsplash.com//photos/random?client_id="
+  const accessKey = "7_3vMLQaq-cRWFvkq2mJwwc7tpeGofkpXvo-jnZqVcM";
+  axios.get(`${apiUrl}${accessKey}&count=${count}`)
+  .then(res => setImages([...images,...res.data]))
+
+}// console.log(images)
   return (
     <div className="App">
    
-
-
-
+<InfiniteScroll dataLength={images.length}
+next={fetchImages}
+hasMore ={true}
+loader={<Loading/>
+  }>
 <UnSplashImages data= {images}/>
+</InfiniteScroll> 
 
 
-
-
-{/* <UnSplashImages data= {images}/>
-<UnSplashImages data= {images}/>
-<UnSplashImages data= {images}/>
-<UnSplashImages data= {images}/>
-<UnSplashImages data= {images}/>
-<UnSplashImages data= {images}/> */}
 </div>
    
 
--- src/components/Loading.js
+++ src/components/Loading.js
@@ -4,6 +4,14 @@ export const Loading = () => {
     return (
         <div>
             <h1>Loader</h1>
+            <div class="loadingio-spinner-bars-8mufvg6axxn">
+                <div class="ldio-jxswtayjiye">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
         </div>
     )
 }
